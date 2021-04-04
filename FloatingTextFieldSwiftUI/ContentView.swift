568d42911974172bb7bd09dc817853d547957700
YidingW <wangyiding.chinese@gmail.com>
2021-04-04 13:41:26 +0800

    Initial Commit

---
+++ FloatingTextFieldSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FloatingTextFieldSwiftUI
+//
+//  Created by 王一丁 on 2021/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FloatingTextFieldSwiftUI/FloatingTextFieldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextFieldSwiftUIApp.swift
+//  FloatingTextFieldSwiftUI
+//
+//  Created by 王一丁 on 2021/4/4.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextFieldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
